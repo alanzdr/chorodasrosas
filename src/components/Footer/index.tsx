78cbae1fe7721801e9580e4fb79935cf181f87d8
Alanderson Zelindro da Rosa <alandersonzelindrodr@gmail.com>
2023-05-16 22:07:54 -0300

    poema: poesia é idiota

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC<Props> = ({ single }) => {
         <h3>Contato</h3>
         <div className={styled.contactList}>
           <a
-            href="https://www.instagram.com/alandersonzdr"
+            href="https://www.instagram.com/chorodasrosas"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -32,27 +32,7 @@ const Footer: React.FC<Props> = ({ single }) => {
               <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
               <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
             </svg>
-            @alandersonzdr
-          </a>
-          <a
-            href="https://twitter.com/AlandersonZdr"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-            </svg>
-            @AlandersonZdr
+            @chorodasrosas
           </a>
           <a
             href="mailto:alandersonzelindrodr@gmail.com"
---
+++ src/data/poesia-e-idiota.md
@@ -0,0 +1,24 @@
+---
+title: "Poesia é idiota"
+thumb: 'https://images.pexels.com/photos/7034134/pexels-photo-7034134.jpeg'
+date: "2023-05-16"
+---
+Poesia é idiota,  
+O que mais poderia ser;  
+Se quem é poeta não percebe,  
+O quão idiota é escrever.  
+<br />
+O amor é idiota,  
+Nada mais poderia ser;  
+Quem ama logo esquece,  
+O quanto de amar também é sofrer.  
+<br />
+Seu sorriso é idiota,  
+Por mais lindo que poderia ser;  
+Ele só me diz que sou fraco,  
+Por todo dia querer ver.  
+<br />
+O futuro é idiota,  
+Se é que poderia ser;  
+Quando alguem idiota como eu,  
+Está perdido de amor por você.  
